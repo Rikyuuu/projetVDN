37ad0b218a47426cc49a4a3e99ad5cdae3eb9fca
Nanotek <couillet.dylan@gmail.com>
2018-04-19 16:44:04 +0200

    ajout header bas

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="style.css" />
+</head>
+    <div>
+        <div>
+            Des <strong>listes de lecture</strong> <br> en harmonie avec votre journée
+        </div>
+
+        <div>
+            L'application La Voix du Nord<br>
+            pour de l'actualité partout, tout le temps
+        </div>
+        <div>
+            Le journal <strong>numérique</strong><br>
+            et ses <strong>suppléments</strong> du week-end
+        </div>
+    </div>
+</html>
